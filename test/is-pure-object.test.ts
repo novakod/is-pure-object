4aba4a85098d7787f2da75ec06a091d5e41cf513
Movsar-Khalakhoev <movsar.kh.128@gmail.com>
2024-04-15 14:06:50 +0300

    Изменения в тестах

--- test/is-pure-object.test.ts
+++ test/is-pure-object.test.ts
@@ -1,23 +1,23 @@
 import { test, expect } from "vitest";
 import { isPureObject } from "../src/is-pure-object";
 
-test("Testing the isPureObject function", () => {
-  expect(isPureObject(new Date()), "testing by date").toBe(false);
-  expect(isPureObject([]), "testing by array").toBe(false);
-  expect(isPureObject(new Array()), "testing by array instance").toBe(false);
-  expect(isPureObject(undefined), "testing by undefined").toBe(false);
-  expect(isPureObject(null), "testing by null").toBe(false);
-  expect(isPureObject(false), "testing by boolean").toBe(false);
-  expect(isPureObject(1), "testing by number").toBe(false);
-  expect(isPureObject("test"), "testing by string").toBe(false);
+test("Тестирование функции isPureObject", () => {
+  expect(isPureObject(new Date()), "Тестирование на дате").toBe(false);
+  expect(isPureObject([]), "Тестирование на массиве").toBe(false);
+  expect(isPureObject(new Array()), "Тестирование на инстансе массива").toBe(false);
+  expect(isPureObject(undefined), "Тестирование на undefined").toBe(false);
+  expect(isPureObject(null), "Тестирование на null").toBe(false);
+  expect(isPureObject(false), "Тестирование на типе boolean").toBe(false);
+  expect(isPureObject(1), "Тестирование на типе number").toBe(false);
+  expect(isPureObject("test"), "Тестирование на типе string").toBe(false);
   expect(
     isPureObject(() => {}),
-    "testing by callback"
+    "Тестирование на коллбэке"
   ).toBe(false);
   expect(
     isPureObject(function () {}),
-    "testing by function"
+    "Тестирование на функции"
   ).toBe(false);
-  expect(isPureObject({}), "testing by object").toBe(true);
-  expect(isPureObject(new Object()), "testing by object instance").toBe(true);
+  expect(isPureObject({}), "Тестирование на объекте").toBe(true);
+  expect(isPureObject(new Object()), "Тестирование на инстансе объекта").toBe(true);
 });
